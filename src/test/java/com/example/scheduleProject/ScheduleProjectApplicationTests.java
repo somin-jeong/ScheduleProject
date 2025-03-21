8caee79f1ee080ccc80e58463a8d638bcf306992
정소민 <yjk70702002@gmail.com>
2025-03-21 19:43:22 +0900

    first commit

---
+++ src/test/java/com/example/scheduleProject/ScheduleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
