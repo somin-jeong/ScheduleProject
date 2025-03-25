6f9aa90bc6e77fd55a8578e456253e28291ee03e
정소민 <yjk70702002@gmail.com>
2025-03-25 21:00:56 +0900

    feat: 일정 삭제 시 비밀번호 필수값 처리

--- src/main/java/com/example/scheduleProject/domain/schedule/dto/request/DeleteScheduleRequestDto.java
+++ src/main/java/com/example/scheduleProject/domain/schedule/dto/request/DeleteScheduleRequestDto.java
@@ -1,7 +1,7 @@
 package com.example.scheduleProject.domain.schedule.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record DeleteScheduleRequestDto(
-        @NotNull String password
+        @NotBlank String password
 ) {}
