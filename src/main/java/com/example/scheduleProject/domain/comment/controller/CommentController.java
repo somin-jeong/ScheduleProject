c1cf25dda591df6f4889cfe88f24c355dbf2ea2e
정소민 <yjk70702002@gmail.com>
2025-04-03 22:20:31 +0900

    feat: CommentController에 JavaDoc 추가

--- src/main/java/com/example/scheduleProject/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/scheduleProject/domain/comment/controller/CommentController.java
@@ -17,6 +17,14 @@ public class CommentController {
     public static final String SESSION_NAME = "loginUser";
     private final CommentService commentService;
 
+    /**
+     * 댓글 생성
+     *
+     * @param userId 유저Id
+     * @param scheduleId 일정Id
+     * @param requestDto (댓글 내용)
+     * @return 생성된 댓글의 id를 포함한 응답 객체
+     */
     @PostMapping("/schedules/{scheduleId}/comments")
     private BaseResponse<SaveCommentResponseDto> saveComment(
             @SessionAttribute(name = SESSION_NAME, required = false) Long userId,
@@ -27,12 +35,26 @@ public class CommentController {
         return new BaseResponse<>(response);
     }
 
+    /**
+     * 댓글 조회
+     *
+     * @param scheduleId 일정Id
+     * @return 댓글(댓글 ID, 내용, 작성자명, 작성일, 수정일) 리스트를 포함한 응답 객체
+     */
     @GetMapping("/schedules/{scheduleId}/comments")
     private BaseResponse<List<CommentResponseDto>> findComment(@PathVariable @Valid Long scheduleId) {
         List<CommentResponseDto> comments = commentService.findComment(scheduleId);
         return new BaseResponse<>(comments);
     }
 
+    /**
+     * 댓글 수정
+     *
+     * @param userId 유저Id
+     * @param commentId 댓글Id
+     * @param requestDto (수정할 댓글 내용)
+     * @return 수정 완료 메시지
+     */
     @PutMapping("/comments/{commentId}")
     private BaseResponse<String> updateComment(
             @SessionAttribute(name = SESSION_NAME, required = false) Long userId,
@@ -40,9 +62,16 @@ public class CommentController {
             @RequestBody @Valid CommentRequestDto requestDto
     ) {
         commentService.updateComment(userId, commentId, requestDto);
-        return new BaseResponse<>("삭제 완료했습니다.");
+        return new BaseResponse<>("수정 완료했습니다.");
     }
 
+    /**
+     * 댓글 삭제
+     *
+     * @param userId 유저Id
+     * @param commentId 댓글Id
+     * @return 삭제 완료 메시지
+     */
     @DeleteMapping("/comments/{commentId}")
     private BaseResponse<String> deleteComment(
             @SessionAttribute(name = SESSION_NAME, required = false) Long userId,
